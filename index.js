9618f1379d0370ed19576c64dfd6a051fe7bbdde
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-09-04 19:46:01 +0700

    Add web server for Render

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Discord Bot đang hoạt động!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Web server đang chạy tại port ${PORT}`);
+});
+
 const {
     Client,
     GatewayIntentBits,
